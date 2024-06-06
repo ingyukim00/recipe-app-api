c518ea2143b5e20e42a4fdcc3c0f8128dc045cfd
ingyukim <ingyukim00@gmail.com>
2024-06-05 22:17:55 -0700

    Add db

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Simple tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers"""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_sub_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.sub(5, 6)
+        self.assertEqual(res, -1)
